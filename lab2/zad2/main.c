379343bbe78435492bc5248e089bee40f79f69f2
Kristina Pavlovska <141680987+pavlovskakristina@users.noreply.github.com>
2026-04-06 19:23:05 +0200

    refactor: Zwiększono czytelność kodu

--- lab2/zad2/main.c
+++ lab2/zad2/main.c
@@ -1,74 +1,64 @@
 #include "stm8s208r.h"
 
+// Numer diody i PIN
+#define D1 5
+#define D2 7
+#define D3 6
+
+#define S1 4
+#define S2 3
+#define S3 2
+
 void main(void)
 {
     // ====== PRZYCISKI ====== //
-    
-    //Przycisk S1
-    PB_DDR &= ~(1<<4);
-    PB_CR1 &= ~(1<<4);
-    PB_CR2 &= ~(1<<4);
-    
-    //Przycisk S2
-    PB_DDR &= ~(1<<3);
-    PB_CR1 &= ~(1<<3);
-    PB_CR2 &= ~(1<<3);
-    
-    //Przycisk S3
-    PB_DDR &= ~(1<<2);
-    PB_CR1 &= ~(1<<2);
-    PB_CR2 &= ~(1<<2);
-    
-    
+
+    PB_DDR &= ~((1 << S1) | (1 << S2) | (1 << S3));
+    PB_CR1 &= ~((1 << S1) | (1 << S2) | (1 << S3));
+    PB_CR2 &= ~((1 << S1) | (1 << S2) | (1 << S3));
+
     // ====== DIODY ====== //
-    //Dioda D1
-    PC_DDR |= (1<<5);
-    PC_CR1 |= (1<<5);
-    PC_CR2 |= (1<<5);
-    
-    //Dioda D2
-    PC_DDR |= (1<<7);
-    PC_CR1 |= (1<<7);
-    PC_CR2 |= (1<<7);
-    
-    //Dioda D3
-    PC_DDR |= (1<<6);
-    PC_CR1 |= (1<<6);
-    PC_CR2 |= (1<<6);
+
+    PC_DDR |= (1 << D1) | (1 << D2) | (1 << D3); // DDR do trzech diod (optymalizacja kodu)
+    PC_CR1 |= (1 << D1) | (1 << D2) | (1 << D3); // CR1 do trzech diod (optymalizacja kodu)
+    PC_CR2 |= (1 << D1) | (1 << D2) | (1 << D3); // CR2 do trzech diod (optymalizacja kodu)
 
     while (1)
     {
         // włączamy S1
-        if ((PB_IDR & (1<<4)) == 0)
+        if ((PB_IDR & (1 << S1)) == 0)
         {
-            PC_ODR  &= ~(1<<5); // włączamy D1
-            
+            PC_ODR &= ~(1 << D1); // włączamy D1
+
             // włączamy S2 i D2
-            if ((PB_IDR & (1<<3)) == 0) // S1, S2 wciśnięte
+            if ((PB_IDR & (1 << S2)) == 0) // S1, S2 wciśnięte
             {
-                PC_ODR  &= ~(1<<7); // włączamy D2
-                
+                PC_ODR &= ~(1 << D2); // włączamy D2
+
                 // włączamy S3 i D3
-                if ((PB_IDR & (1<<2)) == 0) // S1, S2, S3 wciśnięte
+                if ((PB_IDR & (1 << S3)) == 0) // S1, S2, S3 wciśnięte
                 {
-                    PC_ODR  &= ~(1<<6); // włączamy D3
+                    PC_ODR &= ~(1 << D3); // włączamy D3
                 }
-                //wyłączamy S1, S2, S3
-                else { PC_ODR |= (1<<6); } // wyłączamy D3
+                // wyłączamy S1, S2, S3
+                else
+                {
+                    PC_ODR |= (1 << D3);
+                } // wyłączamy D3
             }
-            //wyłączamy S1, S2
+            // wyłączamy S1, S2
             else
             {
-                PC_ODR |= (1<<7); // wyłączamy D2
-                PC_ODR |= (1<<6); // wyłączamy D3
+                PC_ODR |= (1 << D2); // wyłączamy D2
+                PC_ODR |= (1 << D3); // wyłączamy D3
             }
         }
         // wyłączamy S1
         else
         {
-            PC_ODR |= (1<<5); // wyłączamy D1
-            PC_ODR |= (1<<7); // wyłączamy D2
-            PC_ODR |= (1<<6); // wyłączamy D3
+            PC_ODR |= (1 << D1); // wyłączamy D1
+            PC_ODR |= (1 << D2); // wyłączamy D2
+            PC_ODR |= (1 << D3); // wyłączamy D3
         }
     }
 }
